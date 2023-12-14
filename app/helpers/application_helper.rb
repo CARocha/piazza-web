779d96e41b39c9dc8f61e701562cd3dabe672b54
Carlos Rocha <crocha09.09@gmail.com>
2023-12-13 22:23:19 -0600

    add all localize files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# title for each page
 module ApplicationHelper
+  def title
+    return t('piazza') unless content_for?(:title)
+
+    "#{content_for(:title)} | #{t('piazza')}"
+  end
 end
