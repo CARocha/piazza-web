120eea13af95e4719d50cf37a5191e0e754ad7b9
Carlos Rocha <crocha09.09@gmail.com>
2024-01-19 19:55:33 -0600

    add validation password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   before_validation :strip_extraneous_spaces
   validates :name, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true
 
   has_many :memberships, dependent: :destroy
   has_many :organization, through: :memberships
