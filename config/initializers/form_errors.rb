4990edcf8cc5ae94cf6529186ca8fd6f0b65c0e9
Carlos Rocha <crocha09.09@gmail.com>
2024-01-19 19:31:30 -0600

    add custom error

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -3,13 +3,13 @@ ActionView::Base.field_error_proc = -> (html_tag, instance) {
     html = Nokogiri::HTML::DocumentFragment.parse(html_tag)
     html.children.add_class("is-danger")
 
-    error_message_markup = <<~HTML 
+    error_message_markup = <<~HTML
       <p class='help is-danger'>
         #{sanitize(instance.error_message.to_sentence)}
       </p>
     HTML
 
-    "#{html.to_s}#{error_message_markup}".html_safe 
+    "#{html.to_s}#{error_message_markup}".html_safe
   else
     html_tag
   end
